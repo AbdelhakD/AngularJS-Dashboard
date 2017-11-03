8da3090be79540eaccb73da226ddadc51fe7aca3
AbdelhakD <douiri.abdelhak@gmail.com>
2017-11-03 18:06:45 +0100

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
 
@@ -9,6 +10,9 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+
 
     constructor(public router: Router) {
     }
@@ -17,7 +21,21 @@ export class LoginComponent implements OnInit {
     }
 
     onLoggedin() {
+        console.warn(this.model.username);
+        console.warn(this.model.password);
         localStorage.setItem('isLoggedin', 'true');
+        this.loading = true;
+        /*this.loading = true;
+         this.authenticationService.login(this.model.username, this.model.password)
+         .subscribe(
+         data => {
+         this.router.navigate([this.returnUrl]);
+         },
+         error => {
+         this.alertService.error(error);
+         this.loading = false;
+         });*/
+
     }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -7,9 +9,11 @@ import { LoginComponent } from './login.component';
 @NgModule({
     imports: [
         CommonModule,
-        LoginRoutingModule
+        LoginRoutingModule,
+        FormsModule
     ],
     declarations: [LoginComponent]
 })
 export class LoginModule {
+
 }
